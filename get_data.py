300e97925e1c7380e6c488031ee15361f4183a30
Jochen Martin Eppler <j.eppler@fz-juelich.de>
2014-11-11 15:15:57 +0100

    First working copy using a fixed data file and manual parsing of the file.

--- get_data.py
+++ get_data.py
@@ -1,9 +1,61 @@
 import urllib
 import zipfile
 
-fln, headers = urllib.urlretrieve('ftp://ftp-cdc.dwd.de/pub/CDC/observations_germany/climate/daily/kl/recent/tageswerte_KL_00044_akt.zip')
+import os
+import shutil
 
-datazip = zipfile.ZipFile(fln, 'r')
+fname = "tageswerte_KL_00044_akt.zip"
+if not os.path.exists(fname):
+    fln, headers = urllib.urlretrieve('ftp://ftp-cdc.dwd.de/pub/CDC/observations_germany/climate/daily/kl/recent/' + fname)
+    shutil.copyfile(fln, fname)
+
+datazip = zipfile.ZipFile(fname, 'r')
 datazip.namelist()
 
 datacsv = datazip.read('produkt_klima_Tageswerte_20131009_20141108_00044.txt')
+
+datalines = datacsv.splitlines()
+
+header = [item.strip() for item in datalines[0].split(";")]
+
+# remove is in-place
+header.remove('eor')
+
+data = {}
+
+# last line in the file just contains '\x1a', which is not a useful key
+for line in datalines[1:]:
+
+    linedata = line.split(";")
+    if len(linedata) < 2:
+        continue
+
+    daily = {}
+
+    # Don't try to read the last column, as this is always 'eor'
+    for idx, value in enumerate(linedata[:-1]):
+        key = header[idx]
+        if key not in ("Stations_ID", "Mess_Datum"):
+            daily[key] = float(value)
+        elif key == "Mess_Datum":
+            date = value
+
+    data[date] = daily
+
+header.remove("Mess_Datum")
+header.remove("Stations_ID")
+
+timeseries = {}
+for key in header:
+    timeseries[key] = []
+    for date in sorted(data.keys()):
+        timeseries[key].append(data[date][key])
+
+import pylab
+
+datelabels = sorted(data.keys())
+
+pylab.plot(timeseries["LUFTTEMPERATUR"])
+pylab.grid()
+pylab.xticks(range(len(datelabels))[::30], datelabels[::30], rotation=45)
+pylab.show()

b2da0118f775c072c581c7b1591019091096d3fe
Tiziano Zito <opossumnano@gmail.com>
2014-11-24 12:17:12 +0100

    Now we have the data in a dictionary structure

--- get_data.py
+++ get_data.py
@@ -3,9 +3,33 @@ def readin_file(fname):
     flh = open(fname)
     # read the content and store it in a string
     content = flh.read()
+    flh.close()
     return content
 
 myfile = "produkt_klima_Tageswerte_20131023_20141122_00044.txt"
 mydata = readin_file(myfile)
-print(mydata)
+mydata_lines = mydata.splitlines()
+header = mydata_lines[0].split(';')[:-1]
 
+clean_header = []
+for field in header:
+    clean_header.append(field.strip())
+
+header = clean_header
+data = {}
+
+for field in header:
+    data[field] = [] 
+
+for line in mydata_lines[1:]:
+    fields = line.split(';')
+    count = 0
+    for field in fields[:-1]:
+        data_string = field
+        data_value = float(data_string)
+        data[header[count]].append(data_value)
+        count = count + 1
+
+import pylab
+pylab.plot(data['LUFTTEMPERATUR_MAXIMUM'])
+pylab.show()
